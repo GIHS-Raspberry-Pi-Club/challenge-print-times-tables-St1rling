d9593dd2b79b019a10c3f5401fdec36fd8a34e87
Obadiah Crowe <St1rling@users.noreply.github.com>
2017-09-20 16:04:51 +0930

    Made it runnable.

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
-// Created using the GitHub editor. Fight me.
-for (int i = 1; i < 13; i++) {
-  String out = "";
-  for (int j = 1; j < 13; j++) {
-    out = out + (i * j) + " ";
+public class Main {
+  
+  public static void main(String[] args) {
+    // Created using the GitHub editor. Fight me.
+    for (int i = 1; i < 13; i++) {
+      String out = "";
+      for (int j = 1; j < 13; j++) {
+        out = out + (i * j) + " ";
+      }
+      System.out.println(out);
+    }
   }
-  System.out.println(out);
 }
