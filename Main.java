e91abbb6149e42e7182d92f9dc478b223984ee23
Obadiah Crowe <St1rling@users.noreply.github.com>
2017-09-20 16:01:20 +0930

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+// Created using the GitHub editor. Fight me.
+for (int i = 1; i < 13; i++) {
+  String out = "";
+  for (int j = 1; j < 13; j++) {
+    out = out + (i * j) + " ";
+  }
+  System.out.println(out);
+}
